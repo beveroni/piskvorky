e17a8d2299d89cbb565215e9dee114d92a2c8491
Veronika Bendova <svechovaver@gmail.com>
2023-04-10 11:25:35 +0200

    ready for rewiev

--- hra.js
+++ hra.js
@@ -1,22 +1,59 @@
 let currentPlayer = 'circle';
 
-const classAddCircle = (event) => {
-  event.target.classList.add('board__field--circle');
-  const img = document.querySelector('#nowplays');
-  img.src = 'cross-green.svg';
+const playerSwitch = (event) => {
+  if (currentPlayer === 'circle') {
+    event.target.classList.add('board__field--circle');
+    currentPlayer = 'cross';
+    document
+      .querySelector('#nowPlayer')
+      .classList.remove('board__field--circle');
+    document.querySelector('#nowPlayer').classList.add('board__field--cross');
+  } else {
+    event.target.classList.add('board__field--cross');
+    currentPlayer = 'circle';
+    document
+      .querySelector('#nowPlayer')
+      .classList.remove('board__field--cross');
+    document.querySelector('#nowPlayer').classList.add('board__field--circle');
+  }
+
   event.target.disabled = true;
 };
 
-const classAddCross = (event) => {
-  event.target.classList.add('board__field--cross');
-  const img = document.querySelector('#nowplays');
-  img.src = 'circle-purple.svg';
-  event.target.disabled = true;
-};
+document
+  .querySelector('button:nth-child(1)')
+  .addEventListener('click', playerSwitch);
+document
+  .querySelector('button:nth-child(2)')
+  .addEventListener('click', playerSwitch);
+document
+  .querySelector('button:nth-child(3)')
+  .addEventListener('click', playerSwitch);
+document
+  .querySelector('button:nth-child(4)')
+  .addEventListener('click', playerSwitch);
+document
+  .querySelector('button:nth-child(5)')
+  .addEventListener('click', playerSwitch);
+document
+  .querySelector('button:nth-child(6)')
+  .addEventListener('click', playerSwitch);
+document
+  .querySelector('button:nth-child(7)')
+  .addEventListener('click', playerSwitch);
+document
+  .querySelector('button:nth-child(8)')
+  .addEventListener('click', playerSwitch);
+document
+  .querySelector('button:nth-child(9)')
+  .addEventListener('click', playerSwitch);
+document
+  .querySelector('button:nth-child(10)')
+  .addEventListener('click', playerSwitch);
 
 const gameRestart = document.getElementById('restart-game');
 gameRestart.addEventListener('click', function (event) {
-  if (!alert('Chcete opravdu restartovat hru?')) {
+  if (!confirm('Chcete opravdu restartovat hru?')) {
     event.preventDefault();
   }
 });

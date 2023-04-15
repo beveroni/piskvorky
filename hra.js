ca45334b8b4ee6105de5c1c840f128b1c8d70db4
Veronika Bendova <svechovaver@gmail.com>
2023-04-15 18:46:57 +0200

    rapair cell-grid opacity and add queryslector for all buttons and click event for all buttons

--- hra.js
+++ hra.js
@@ -1,4 +1,7 @@
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
+
 let currentPlayer = 'circle';
+// let buttons = '';
 
 const playerSwitch = (event) => {
   if (currentPlayer === 'circle') {
@@ -20,40 +23,46 @@ const playerSwitch = (event) => {
   event.target.disabled = true;
 };
 
-document
-  .querySelector('button:nth-child(1)')
-  .addEventListener('click', playerSwitch);
-document
-  .querySelector('button:nth-child(2)')
-  .addEventListener('click', playerSwitch);
-document
-  .querySelector('button:nth-child(3)')
-  .addEventListener('click', playerSwitch);
-document
-  .querySelector('button:nth-child(4)')
-  .addEventListener('click', playerSwitch);
-document
-  .querySelector('button:nth-child(5)')
-  .addEventListener('click', playerSwitch);
-document
-  .querySelector('button:nth-child(6)')
-  .addEventListener('click', playerSwitch);
-document
-  .querySelector('button:nth-child(7)')
-  .addEventListener('click', playerSwitch);
-document
-  .querySelector('button:nth-child(8)')
-  .addEventListener('click', playerSwitch);
-document
-  .querySelector('button:nth-child(9)')
-  .addEventListener('click', playerSwitch);
-document
-  .querySelector('button:nth-child(10)')
-  .addEventListener('click', playerSwitch);
+// document
+//   .querySelector('button:nth-child(1)')
+//   .addEventListener('click', playerSwitch);
+// document
+//   .querySelector('button:nth-child(2)')
+//   .addEventListener('click', playerSwitch);
+// document
+//   .querySelector('button:nth-child(3)')
+//   .addEventListener('click', playerSwitch);
+// document
+//   .querySelector('button:nth-child(4)')
+//   .addEventListener('click', playerSwitch);
+// document
+//   .querySelector('button:nth-child(5)')
+//   .addEventListener('click', playerSwitch);
+// document
+//   .querySelector('button:nth-child(6)')
+//   .addEventListener('click', playerSwitch);
+// document
+//   .querySelector('button:nth-child(7)')
+//   .addEventListener('click', playerSwitch);
+// document
+//   .querySelector('button:nth-child(8)')
+//   .addEventListener('click', playerSwitch);
+// document
+//   .querySelector('button:nth-child(9)')
+//   .addEventListener('click', playerSwitch);
+// document
+//   .querySelector('button:nth-child(10)')
+//   .addEventListener('click', playerSwitch);
 
 const gameRestart = document.getElementById('restart-game');
 gameRestart.addEventListener('click', function (event) {
   if (!confirm('Chcete opravdu restartovat hru?')) {
     event.preventDefault();
   }
+});
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', playerSwitch);
 });

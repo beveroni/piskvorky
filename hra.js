c905ea876e32d0e694c3253d79a7f6dcc1484f0f
Veronika Bendova <svechovaver@gmail.com>
2023-04-15 20:26:54 +0200

    add findWinner function, setTimeouts

--- hra.js
+++ hra.js
@@ -3,6 +3,13 @@ import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 let currentPlayer = 'circle';
 let button = '_';
 
+const gameRestart = document.getElementById('restart-game');
+gameRestart.addEventListener('click', function (event) {
+  if (!confirm('Chcete opravdu restartovat hru?')) {
+    event.preventDefault();
+  }
+});
+
 const gameField = [
   '_',
   '_',
@@ -106,37 +113,49 @@ const gameField = [
   '_',
 ];
 
-const playerSwitch = (event) => {
-  if (currentPlayer === 'circle') {
-    event.target.classList.add('board__field--circle');
-    currentPlayer = 'cross';
-    gameField[index] = 'o';
-    document
-      .querySelector('#nowPlayer')
-      .classList.remove('board__field--circle');
-    document.querySelector('#nowPlayer').classList.add('board__field--cross');
-  } else {
-    gameField[index] = 'x';
-    event.target.classList.add('board__field--cross');
-    currentPlayer = 'circle';
-    document
-      .querySelector('#nowPlayer')
-      .classList.remove('board__field--cross');
-    document.querySelector('#nowPlayer').classList.add('board__field--circle');
-  }
-
-  event.target.disabled = true;
-};
-
-const gameRestart = document.getElementById('restart-game');
-gameRestart.addEventListener('click', function (event) {
-  if (!confirm('Chcete opravdu restartovat hru?')) {
-    event.preventDefault();
-  }
-});
-
 const buttons = document.querySelectorAll('button');
+buttons.forEach((button, i) => {
+  const playerSwitch = (e) => {
+    if (currentPlayer === 'circle') {
+      e.target.classList.add('board__field--circle');
+      currentPlayer = 'cross';
+      gameField[i] = 'o';
+      document
+        .querySelector('#nowPlayer')
+        .classList.remove('board__field--circle');
+      document.querySelector('#nowPlayer').classList.add('board__field--cross');
+    } else {
+      gameField[i] = 'x';
+      e.target.classList.add('board__field--cross');
+      currentPlayer = 'circle';
+      document
+        .querySelector('#nowPlayer')
+        .classList.remove('board__field--cross');
+      document
+        .querySelector('#nowPlayer')
+        .classList.add('board__field--circle');
+    }
 
-buttons.forEach((button, index) => {
+    e.target.disabled = true;
+
+    const winner = findWinner(gameField);
+    if (winner === 'o') {
+      setTimeout(() => {
+        alert(`Vyhrálo ⭕`);
+        location.reload();
+      }, 250);
+    } else if (winner === 'x') {
+      setTimeout(() => {
+        alert(`Vyhrál ❌`);
+        location.reload();
+      }, 250);
+    } else if (winner === 'tie') {
+      setTimeout(() => {
+        alert('Hra skončila nerozhodně.');
+        location.reload();
+      }, 250);
+    }
+  };
+
   button.addEventListener('click', playerSwitch);
 });
